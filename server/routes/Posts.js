29a5256b29c4cd9117a1501b5f3d9cf5443f5d6d
keremalpdag <kalpdag@gmail.com>
2023-09-19 04:25:17 +0300

    project upload

---
+++ server/routes/Posts.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const { Posts, Likes } = require("../models");
+const { validateToken } = require('../middlewares/AuthMiddleware')
+
+router.get("/", async (req, res) => {
+  const listOfPosts = await Posts.findAll({ include: [Likes] });
+  res.json(listOfPosts);
+});
+
+router.get("/byId/:id", async (req, res) => {
+  const id = req.params.id;
+  const post = await Posts.findByPk(id);
+  res.json(post);
+});
+
+router.post("/", validateToken, async (req, res) => {
+  const post = req.body;
+  post.username = req.user.username;
+  await Posts.create(post);
+  res.json(post);
+});
+
+router.delete("/:id", validateToken, async (req, res) => {
+  const postId = req.params.id;
+
+  await Posts.destroy({
+    where: {
+      id: postId,
+    }
+  });
+
+  res.json("deleted");
+})
+
+module.exports = router;
